330f39c466ac768bfaca4033cb5b2003666edcc3
Robert Mischie <you@example.com>
2020-11-30 22:06:07 +0000

    Changed title name

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Robert Portofolio`,
+        short_name: `Portofolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
